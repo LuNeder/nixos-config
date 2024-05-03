028b347291000ee4b89fa17cae15601ba45dec79
Luana <luana@luana.dev.br>
2024-05-03 12:09:57 -0300

    Ok but why tf is the kernel so fucking outdated in a supposedly rolling release system?

--- virtualbox/nixos/configuration.nix
+++ virtualbox/nixos/configuration.nix
@@ -152,8 +152,9 @@
   # $ nix search wget
   environment.systemPackages = [
   #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-    pkgsMusl.curl
-    pkgsMusl.git
+    pkgsGnu.curl
+    pkgsGnu.git
+   # pkgsGnu.authy
   ];
 
   # Some programs need SUID wrappers, can be configured further or are

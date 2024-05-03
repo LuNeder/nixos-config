5d61d58fb070459c57e14db1cb8e782e0c3d19cc
Luana <luana@luana.dev.br>
2024-05-03 10:56:12 -0300

    a

--- flake.nix
+++ flake.nix
@@ -4,12 +4,14 @@
   outputs = { self, nixpkgs, ... } @ inputs: let inherit (self) outputs;
   in {
     nixosConfigurations = {
-      virtualbox = let pkgsMusl = import nixpkgs { system = "86_64-unknown-linux-musl"; };
-        pkgsGnu = import nixpkgs { system = "x86_64-linux"; };
+      virtualbox = (
+        let 
+         pkgsMusl = import nixpkgs { system = "86_64-unknown-linux-musl"; };
+         pkgsGnu = import nixpkgs { system = "x86_64-linux"; };
         in nixpkgs.lib.nixosSystem {
           specialArgs = {inherit inputs outputs pkgsMusl;};
           modules = [./nixos/configuration.nix];
-        };
+        });
     };
   };
 }

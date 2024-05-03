91b830e84eb04f1b6a15f22f1123249f236feb38
Luana <luana@luana.dev.br>
2024-05-03 12:21:00 -0300

    a

--- virtualbox/nixos/configuration.nix
+++ virtualbox/nixos/configuration.nix
@@ -96,6 +96,8 @@
   # Enable the XFCE Desktop Environment.
   services.xserver.displayManager.lightdm.enable = true;
   services.xserver.desktopManager.xfce.enable = true;
+  # Compiz
+  services.xserver.windowManager.compiz.enable = true;
 
   # Configure keymap in X11
   services.xserver = {

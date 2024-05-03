cb7868c252478818d36a256719c6e500cda8d7b6
Luana <luana@luana.dev.br>
2024-05-03 11:40:03 -0300

    flake

--- flake.nix
+++ flake.nix
@@ -1,17 +1,23 @@
 {
   inputs = { nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable"; };
 
-  outputs = { self, nixpkgs, ... } @ inputs: let inherit (self) outputs;
-  in {
-    nixosConfigurations = {
-      virtualbox = (
-        let 
-         pkgsMusl = import nixpkgs { config = "86_64-unknown-linux-musl"; };
-         pkgsGnu = import nixpkgs { system = "x86_64-linux"; };
-        in nixpkgs.lib.nixosSystem {
-          specialArgs = {inherit inputs outputs pkgsMusl;};
-          modules = [./nixos/configuration.nix];
+  outputs = { self, nixpkgs, ... } @ inputs: 
+    let 
+      inherit (self) outputs;
+      pkgsMusl = import nixpkgs { config = "86_64-unknown-linux-musl"; };
+      pkgsGnu = import nixpkgs { system = "x86_64-linux"; };
+    in {
+      nixosConfigurations = {
+        virtualbox = ( nixpkgs.lib.nixosSystem {
+            specialArgs = {inherit inputs outputs pkgsGnu pkgsMusl;};
+            modules = [./virtualbox/nixos/configuration.nix];
         });
+        #Luana-X670E = (
+
+        #);
+        #Luana-Legion-5 = (
+
+        #);
+      };
     };
-  };
 }

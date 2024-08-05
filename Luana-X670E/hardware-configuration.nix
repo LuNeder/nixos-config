454a0d81247b8ada2aa765a42e222e469462dc13
Luana <luana@luana.dev.br>
2024-08-05 15:50:36 -0300

    fix swap

--- Luana-X670E/hardware-configuration.nix
+++ Luana-X670E/hardware-configuration.nix
@@ -30,7 +30,7 @@
       fsType = "btrfs";
     };
 
-  swapDevices = [ ];
+  swapDevices = [ {device = "/dev/nvme0n1p1";} ];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's

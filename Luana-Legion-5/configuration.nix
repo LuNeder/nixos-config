9c310593cfe3c94aa0b9ec88651a90d7767fada8
Luana <luana@luana.dev.br>
2024-09-04 21:59:08 -0300

    software-center works! snow doesnt conf-editor works but isnt very intuitive

--- Luana-Legion-5/configuration.nix
+++ Luana-Legion-5/configuration.nix
@@ -53,6 +53,7 @@
   inputs.nix-software-center.packages.${pkgs.system}.nix-software-center
   inputs.nixos-conf-editor.packages.${pkgs.system}.nixos-conf-editor
   inputs.snow.packages.${pkgs.system}.snow
+    pkgs.gparted
   ];
 
   # Steam

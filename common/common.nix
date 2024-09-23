ece13b3f817bcda96bf75226962fb4c49532599f
Luana <luana@luana.dev.br>
2024-09-23 18:31:15 -0300

    Fix the impurity readded by the refactor

--- common/common.nix
+++ common/common.nix
@@ -4,15 +4,14 @@
   nixpkgs.config.allowUnfree = true; # Allow unfree packages
   nixpkgs.config.cudaSupport = true; # Enable CUDA
   nixpkgs.overlays = [(final: prev: {
-    # TODO: localSystem.system = final.stdenv.hostPlatform; fails for some reason, cannot coerce set to string or something
-      # due to this, the error: attribute 'currentSystem' missing impurity is back
-    # pkgsMusl = import inputs.nixpkgs { config.allowUnfree = true;  hostPlatform.config = "x86_64-unknown-linux-musl";}; # config.cudaSupport = true; config.cudaVersion = "12";}; 
-    pkgsGnu = import inputs.nixpkgs {  config.allowUnfree = true;  hostPlatform.config = "x86_64-unknown-linux-gnu"; config.cudaSupport = true; config.cudaVersion = "12";}; 
-    pkgsNoCu = import inputs.nixpkgs { config.allowUnfree = true;  hostPlatform.config = "x86_64-unknown-linux-musl"; }; # TODO: Nix ignores when I change this to musl...
-    # pkgsOld = import inputs.pkgs-old { config.allowUnfree = true;  hostPlatform.config = "x86_64-unknown-linux-musl"; }; 
-    pkgsWivrn = import inputs.pkgs-wivrn { config.allowUnfree = true;  hostPlatform.config = "x86_64-unknown-linux-musl"; config.cudaSupport = true; config.cudaVersion = "12";}; 
-    pkgsmndvlknlyrs = import inputs.pkgs-mndvlknlyrs { config.allowUnfree = true;  hostPlatform.config = "x86_64-unknown-linux-musl"; config.cudaSupport = false;}; # TODO: broken due to opencv, add cuda
-    pkgsAlvr = import inputs.pkgs-alvr { config.allowUnfree = true;  hostPlatform.config = "x86_64-unknown-linux-musl"; config.cudaSupport = true; config.cudaVersion = "12";};
+    # TODO: unhardcode hostPlatform.config (how to unhardcode the x86_64 part without removing -musl?)
+    # pkgsMusl = import inputs.nixpkgs { config.allowUnfree = true;  localSystem.system = final.stdenv.hostPlatform.system; hostPlatform.config = "x86_64-unknown-linux-musl";}; # config.cudaSupport = true; config.cudaVersion = "12";}; 
+    pkgsGnu = import inputs.nixpkgs {  config.allowUnfree = true;  localSystem.system = final.stdenv.hostPlatform.system; hostPlatform.config = "x86_64-unknown-linux-gnu"; config.cudaSupport = true; config.cudaVersion = "12";}; 
+    pkgsNoCu = import inputs.nixpkgs { config.allowUnfree = true;  localSystem.system = final.stdenv.hostPlatform.system; hostPlatform.config = "x86_64-unknown-linux-musl"; }; # TODO: Nix ignores when I change this to musl...
+    # pkgsOld = import inputs.pkgs-old { config.allowUnfree = true;  localSystem.system = final.stdenv.hostPlatform.system; hostPlatform.config = "x86_64-unknown-linux-musl"; }; 
+    pkgsWivrn = import inputs.pkgs-wivrn { config.allowUnfree = true;  localSystem.system = final.stdenv.hostPlatform.system; hostPlatform.config = "x86_64-unknown-linux-musl"; config.cudaSupport = true; config.cudaVersion = "12";}; 
+    pkgsmndvlknlyrs = import inputs.pkgs-mndvlknlyrs { config.allowUnfree = true;  localSystem.system = final.stdenv.hostPlatform.system; hostPlatform.config = "x86_64-unknown-linux-musl"; config.cudaSupport = false;}; # TODO: broken due to opencv, add cuda
+    pkgsAlvr = import inputs.pkgs-alvr { config.allowUnfree = true;  localSystem.system = final.stdenv.hostPlatform.system; hostPlatform.config = "x86_64-unknown-linux-musl"; config.cudaSupport = true; config.cudaVersion = "12";};
   })];
 
   # Enable sysrq keys that for some dumb reason come disabled by default

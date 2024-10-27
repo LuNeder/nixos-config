4118e1a788132a82f838e34c1299dc9e36021331
Luana <luana@luana.dev.br>
2024-10-27 16:08:51 -0300

    What the fuck, it works here??
    https://github.com/NixOS/nixpkgs/pull/308097
    https://github.com/alvr-org/ALVR/issues/2476
    https://github.com/ValveSoftware/SteamVR-for-Linux/issues/748
    https://github.com/NixOS/nixpkgs/pull/341219

--- flake.nix
+++ flake.nix
@@ -7,7 +7,7 @@
       "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
     ];
   };
-  inputs = { nixpkgs.url = "github:nixos/nixpkgs/1d76033154a0b7498c423244f90fb5ecfa62ca38"; # TODO: Revert to unstable when steamvr is fixed
+  inputs = { nixpkgs.url = "github:nixos/nixpkgs/fb4afaa052082fe0d9f5a325047deb429e782bdf"; # TODO: Revert to unstable when steamvr is fixed
     pkgs-old.url = "github:nixos/nixpkgs/nixos-23.11";
     pkgs-wivrn.url = "github:PassiveLemon/nixpkgs/wivrn-init"; # TODO: merged, remove
     pkgs-mndvlknlyrs.url = "github:Scrumplex/nixpkgs/nixos/monado/vulkan-layers"; # TODO: remove when merged

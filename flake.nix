b4fe3188d903b77cbf5d0ae466bf47f768627cfa
Luana <luana@luana.dev.br>
2024-10-27 18:14:05 -0300

    Linux 6.11.5 seems to have fixed Bluez
    (https://github.com/bluez/bluez/issues/988)

--- flake.nix
+++ flake.nix
@@ -22,7 +22,6 @@
     nix-software-center.url = "github:snowfallorg/nix-software-center";
     nixos-conf-editor.url = "github:snowfallorg/nixos-conf-editor";
     snow.url = "github:snowfallorg/snow";
-    pkgs-kernel.url = "github:nixos/nixpkgs/3029741718f4c765fbc5ebf76bea3d6c8ff15fe5"; # TODO: remove when bluez/bluez#988 is fixed
   };
 
   

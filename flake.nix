765e830ca0d72ce2dcdf014dadabede009903723
Luana <luana@luana.dev.br>
2024-10-27 16:55:48 -0300

    Broken here
    ugh, at this point I just give up. I'll try doing this a different method or something, why tf do some commits have two parents I hate git
    
    https://github.com/NixOS/nixpkgs/pull/308097
    https://github.com/alvr-org/ALVR/issues/2476
    https://github.com/ValveSoftware/SteamVR-for-Linux/issues/748
    https://github.com/NixOS/nixpkgs/pull/341219

--- flake.nix
+++ flake.nix
@@ -7,7 +7,7 @@
       "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
     ];
   };
-  inputs = { nixpkgs.url = "github:nixos/nixpkgs/f6c921ee279b603896b341382384d4d2be0d2ae5"; # TODO: Revert to unstable when steamvr is fixed
+  inputs = { nixpkgs.url = "github:nixos/nixpkgs/ab0e8e5be7e62ae239c47f8fc8ed645dce40cc29"; # TODO: Revert to unstable when steamvr is fixed
     pkgs-old.url = "github:nixos/nixpkgs/nixos-23.11";
     pkgs-wivrn.url = "github:PassiveLemon/nixpkgs/wivrn-init"; # TODO: merged, remove
     pkgs-mndvlknlyrs.url = "github:Scrumplex/nixpkgs/nixos/monado/vulkan-layers"; # TODO: remove when merged

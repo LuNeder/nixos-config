33b695985512dcab1d49766a3dd838e484d3ba11
Luana <luana@luana.dev.br>
2024-10-27 15:29:45 -0300

    ...and works here. The culprit was merged to nixpkgs somewhere between oct1 and oct8.
    https://github.com/NixOS/nixpkgs/pull/308097
    https://github.com/alvr-org/ALVR/issues/2476

--- flake.nix
+++ flake.nix
@@ -7,7 +7,7 @@
       "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
     ];
   };
-  inputs = { nixpkgs.url = "github:nixos/nixpkgs/273673e839189c26130d48993d849a84199523e6"; # TODO: Revert to unstable when steamvr is fixed
+  inputs = { nixpkgs.url = "github:nixos/nixpkgs/27e30d177e57d912d614c88c622dcfdb2e6e6515"; # TODO: Revert to unstable when steamvr is fixed
     pkgs-old.url = "github:nixos/nixpkgs/nixos-23.11";
     pkgs-wivrn.url = "github:PassiveLemon/nixpkgs/wivrn-init"; # TODO: merged, remove
     pkgs-mndvlknlyrs.url = "github:Scrumplex/nixpkgs/nixos/monado/vulkan-layers"; # TODO: remove when merged

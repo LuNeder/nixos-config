00e26470857f21ee5c8644afcd849a385dbc1b63
Luana <luana@luana.dev.br>
2024-10-30 10:41:53 -0300

    Re-testing: indeed still broken on NixOS/nixpkgs#351928
    https://github.com/NixOS/nixpkgs/issues/352304

--- flake.nix
+++ flake.nix
@@ -7,7 +7,7 @@
       "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
     ];
   };
-  inputs = { nixpkgs.url = "github:NixOS/nixpkgs/273673e839189c26130d48993d849a84199523e6"; # TODO: go back to upstream when SteamVR/ALVR Fixed
+  inputs = { nixpkgs.url = "github:K900/nixpkgs/awful-steamage"; # TODO: go back to unstable when SteamVR/ALVR fixed (NixOS/nixpkgs#352304)
     pkgs-old.url = "github:nixos/nixpkgs/nixos-23.11";
     pkgs-mndvlknlyrs.url = "github:Scrumplex/nixpkgs/nixos/monado/vulkan-layers"; # TODO: remove when merged
     pkgs-alvr.url = "github:jopejoe1/nixpkgs/alvr-src"; # TODO: remove when merged

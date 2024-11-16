eb207302415283b66f48745fa77a71470e39f726
Luana <luana@luana.dev.br>
2024-11-16 20:00:03 -0300

    Dup + Cleanup + COSMIC (I'll try it out) + overlay opencv

--- Luana-X670E/configuration.nix
+++ Luana-X670E/configuration.nix
@@ -11,7 +11,7 @@
       inputs.home-manager.nixosModules.home-manager # Home Manager
       ./hardware-configuration.nix
       # ./gpu-passthrough.nix
-      "${inputs.porn-vault}/nixos/modules/services/web-apps/porn-vault/default.nix"
+      inputs.nixos-cosmic.nixosModules.default
     ];
 
 # Broken due to uutils issue #6351 # TODO: Wait for fix  # No GNU on this house! Use Uutils instead of GNU coreutils
@@ -256,14 +256,6 @@
     pkgs.heroic
   ];
 
-  services.porn-vault = {
-    enable = true;
-    package = inputs.porn-vault.legacyPackages.${pkgs.system}.porn-vault;
-    autoStart = true;
-    settings.enable = true;
-    openFirewall = true;
-  };
-
   programs.criu.enable = true;
 
   services.usbmuxd = {
@@ -325,7 +317,7 @@
   # Run normal binaries
   programs.nix-ld.enable = false;
   programs.nix-ld.libraries = [config.boot.kernelPackages.nvidiaPackages.stable] ++ (with pkgs; [
-    libva # fuck alvr, they removed the appimages
+    libva
     ocamlPackages.alsa
     alsa-lib
     xfce.libxfce4windowing
@@ -416,6 +408,10 @@
   services.xserver.displayManager.lightdm.enable = true;
   services.xserver.desktopManager.xfce.enable = true;
   programs.xfconf.enable = true;
+
+  # Enable the COSMIC Desktop Environment.
+  services.desktopManager.cosmic.enable = true;
+
   
   # Desktop Configuration
   services.bamf.enable = true; # needed for Plank bc nix dumb nixpkgs#42873
--- flake.nix
+++ flake.nix
@@ -7,11 +7,9 @@
       "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
     ];
   };
-  inputs = { nixpkgs.url = "github:LuNeder/nixpkgs/nixos-unstable"; # TODO: go back to unstable when SteamVR/ALVR fixed (NixOS/nixpkgs#352304)
-    porn-vault.url = "/home/luana/Documentos/GitHub/nixpkgs/";  
+  inputs = { nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixos-cosmic.url = "github:lilyinstarlight/nixos-cosmic";  
     pkgs-old.url = "github:nixos/nixpkgs/nixos-23.11";
-    pkgs-mndvlknlyrs.url = "github:Scrumplex/nixpkgs/nixos/monado/vulkan-layers"; # TODO: merged, remove
-    pkgs-alvr.url = "github:LuNeder/nixpkgs/alvrplusk900"; # TODO: remove when merged
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     systems.url = "github:nix-systems/default-linux";
@@ -27,7 +25,7 @@
 
   
 
-  outputs = { self, nixpkgs, pkgs-old, pkgs-mndvlknlyrs, pkgs-alvr, systems, nix-flatpak, home-manager, ... } @ inputs: 
+  outputs = { self, nixpkgs, systems, nix-flatpak, home-manager, ... } @ inputs: 
     let
       inherit (self) outputs;
       lib = nixpkgs.lib // home-manager.lib; 

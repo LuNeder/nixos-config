405a2d1a4d18f6269fe9bc5cba44dc0588531939
Luana <luana@luana.dev.br>
2025-02-25 11:02:53 -0300

    Done (the update script I needed to run was fixed after a pr I made was merged but I was still on a old commit locally)

--- Luana-X670E/configuration.nix
+++ Luana-X670E/configuration.nix
@@ -389,11 +389,6 @@
     libsForQt5.qwt6_1
   ]);
 
-  # Symbolic Links
-  systemd.tmpfiles.rules = [
-    "L+ /bin/bash - - - - ${pkgs.bash}/bin/bash"
-  ];
-
   # Flatpaks (enabled in common.nix)
   services.flatpak.update.onActivation = true;
   services.flatpak.remotes = [

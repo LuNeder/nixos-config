f5161c652c44bf71a0e7e63baa70a98fea0fdb77
Luana <luana@luana.dev.br>
2025-03-14 16:37:15 -0300

    Dup: Test new alvr version, still works!

--- flake.nix
+++ flake.nix
@@ -7,7 +7,7 @@
       "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
     ];
   };
-  inputs = { nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable"; 
+  inputs = { nixpkgs.url = "github:r-ryantm/nixpkgs/auto-update/alvr"; 
 
     zen-browser.url = "github:youwen5/zen-browser-flake";
     zen-browser.inputs.nixpkgs.follows = "nixpkgs";

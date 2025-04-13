7ae704009941ade5b3dd162540d458d3e918729a
Luana <luana@luana.dev.br>
2025-04-13 18:43:42 -0300

    Enable gc

--- common/common.nix
+++ common/common.nix
@@ -19,9 +19,9 @@
   # Garbage Collector
   nix.settings.auto-optimise-store = true;
   nix.gc = {
-    automatic = false; # TODO: Reenable when everything the oct1 (gen 255) -> oct20 (gen 256) update broke is fixed (sep28 dup -> oct20 dup actually)
-    #dates = "weekly";
-    #options = "-d --delete-older-than 30d";
+    automatic = true;
+    dates = "weekly";
+    options = "-d --delete-older-than 30d";
   };
 
   # Flakes

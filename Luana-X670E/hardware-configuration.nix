9ecdf708d0334d28b3ed5f36d37cd8a7c4962832
Luana <luana@luana.dev.br>
2025-05-07 13:19:23 -0300

    NAS and PC: Thunderbolt

--- Luana-X670E/hardware-configuration.nix
+++ Luana-X670E/hardware-configuration.nix
@@ -37,6 +37,7 @@
   # still possible to use this option, but it's recommended to use it in conjunction
   # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
   networking.useDHCP = lib.mkDefault true;
+  networking.interfaces.thunderbolt0.useDHCP = lib.mkForce false;
   # networking.interfaces.enp77s0.useDHCP = lib.mkDefault true;
   # networking.interfaces.wlp76s0.useDHCP = lib.mkDefault true;
 
--- Yoke/hardware-configuration.nix
+++ Yoke/hardware-configuration.nix
@@ -51,6 +51,7 @@
   # still possible to use this option, but it's recommended to use it in conjunction
   # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
   networking.useDHCP = lib.mkDefault true;
+  networking.interfaces.thunderbolt0.useDHCP = lib.mkForce false;
   # networking.interfaces.eno1.useDHCP = lib.mkDefault true;
   # networking.interfaces.wlp95s0.useDHCP = lib.mkDefault true;
 

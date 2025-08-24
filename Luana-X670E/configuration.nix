edbe0679bb18a1ce0992a322fffe0d842087a69b
Luana <luana@luana.dev.br>
2025-08-24 08:10:04 -0300

    feat: VirtualBox

--- Luana-X670E/configuration.nix
+++ Luana-X670E/configuration.nix
@@ -742,12 +742,12 @@
   # services.transmission.enable = true;
 
   # VirtualBox
-  #virtualisation.virtualbox.host.enable = true;
+  virtualisation.virtualbox.host.enable = true;
   users.extraGroups.vboxusers.members = [ "luana" ];
-  #virtualisation.virtualbox.host.enableExtensionPack = true;
-  # virtualisation.virtualbox.host.enableKvm = true; ##
-  #virtualisation.virtualbox.host.enableHardening = false;
-  # virtualisation.virtualbox.host.addNetworkInterface = false;
+  virtualisation.virtualbox.host.enableExtensionPack = true;
+  virtualisation.virtualbox.host.enableKvm = true; ##
+  virtualisation.virtualbox.host.enableHardening = false;
+  virtualisation.virtualbox.host.addNetworkInterface = false; ##
 
   programs.virt-manager.enable = true;
 

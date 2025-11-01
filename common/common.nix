b59e269b8511803d007123b96e567f9e3f82bbb3
Luana <luana@luana.dev.br>
2025-11-01 10:56:53 -0300

    fix: pkgs.hostPlatform alias
    Nixpkgs maintainers are fucking around again. Two occurences on the same day, even. What a surprise.
    (https://github.com/NixOS/nixpkgs/pull/456527)
    
    In true nixpkgs fashion, we don't even get a decent error message for that.
    
    ```
           error: attempt to call something which is not a function but a string: "linuxArch"
           at /nix/store/637jq68bl0pwqkiv2ms2p58x3mfpnhz6-source/common/common.nix:13:178:
               12|       # TODO: check if this unhardcoding of the arch in hostPlatform.config actually works
               13|       pkgsGnu = import inputs.nixpkgs {  config.allowUnfree = config.nixpkgs.config.allowUnfree;  localSystem.system = final.stdenv.hostPlatform.system; localSystem.config = "${pkgs.hostPlatform.linuxArch}-unknown-linux-gnu"; config.cudaSupport = true; config.cudaVersion = "12";};
                 |                                                                                                                                                                                  ^
    ```
    
    Still getting a 'evaluation warning: 'system' has been renamed to/replaced by 'stdenv.hostPlatform.system'' now but idk where tf I'm using 'system' on my config...

--- common/common.nix
+++ common/common.nix
@@ -10,10 +10,10 @@
   nixpkgs.overlays = [
     (final: prev: {
       # TODO: check if this unhardcoding of the arch in hostPlatform.config actually works
-      pkgsGnu = import inputs.nixpkgs {  config.allowUnfree = config.nixpkgs.config.allowUnfree;  localSystem.system = final.stdenv.hostPlatform.system; localSystem.config = "${pkgs.hostPlatform.linuxArch}-unknown-linux-gnu"; config.cudaSupport = true; config.cudaVersion = "12";}; 
-      # pkgsMusl = import inputs.nixpkgs { config.allowUnfree = config.nixpkgs.config.allowUnfree;  localSystem.system = final.stdenv.hostPlatform.system; localSystem.config = "${pkgs.hostPlatform.linuxArch}-unknown-linux-musl";}; # config.cudaSupport = true; config.cudaVersion = "12";}; 
-      pkgsNoCu = import inputs.nixpkgs { config.allowUnfree = config.nixpkgs.config.allowUnfree;  localSystem.system = final.stdenv.hostPlatform.system; localSystem.config = "${pkgs.hostPlatform.config}"; }; # TODO: Nix ignores when I change this to musl...
-      pkgsCu = import inputs.nixpkgs {  config.allowUnfree = config.nixpkgs.config.allowUnfree;  localSystem.system = final.stdenv.hostPlatform.system; localSystem.config = "${pkgs.hostPlatform.config}"; config.cudaSupport = true; config.cudaVersion = "12";};
+      pkgsGnu = import inputs.nixpkgs {  config.allowUnfree = config.nixpkgs.config.allowUnfree;  localSystem.system = final.stdenv.hostPlatform.system; localSystem.config = "${final.stdenv.hostPlatform.linuxArch}-unknown-linux-gnu"; config.cudaSupport = true; config.cudaVersion = "12";}; 
+      # pkgsMusl = import inputs.nixpkgs { config.allowUnfree = config.nixpkgs.config.allowUnfree;  localSystem.system = final.stdenv.hostPlatform.system; localSystem.config = "${final.stdenv.hostPlatform.linuxArch}-unknown-linux-musl";}; # config.cudaSupport = true; config.cudaVersion = "12";}; 
+      pkgsNoCu = import inputs.nixpkgs { config.allowUnfree = config.nixpkgs.config.allowUnfree;  localSystem.system = final.stdenv.hostPlatform.system; localSystem.config = "${final.stdenv.hostPlatform.config}"; }; # TODO: Nix ignores when I change this to musl...
+      pkgsCu = import inputs.nixpkgs {  config.allowUnfree = config.nixpkgs.config.allowUnfree;  localSystem.system = final.stdenv.hostPlatform.system; localSystem.config = "${final.stdenv.hostPlatform.config}"; config.cudaSupport = true; config.cudaVersion = "12";}; # For machines without full cudaSupport enabled
     })
   ];
 

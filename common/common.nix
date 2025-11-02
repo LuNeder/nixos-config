6b7633934a8f4731031f5a8a87aa171e7035b4b9
Luana <luana@luana.dev.br>
2025-11-02 12:30:48 -0300

    feat: fwupd

--- common/common.nix
+++ common/common.nix
@@ -181,6 +181,9 @@
 sops.defaultSopsFile = ../secrets/secrets.yaml;
 sops.age.sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
 
+# fwupd
+services.fwupd.enable = true;
+
 # Needed by ulauncher and heroic-launcher
 nixpkgs.config.permittedInsecurePackages = [
   "libsoup-2.74.3"

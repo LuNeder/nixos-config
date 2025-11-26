cc1c63cbdf08cfbfb195818406ba9eba954cf31f
Luana <luana@luana.dev.br>
2025-11-26 17:40:45 -0300

    feat: Use my cache

--- flake.nix
+++ flake.nix
@@ -1,10 +1,10 @@
 {
   nixConfig = {
-    extra-substituters = [
-      "https://cuda-maintainers.cachix.org"
+    substituters = [
+      "http://100.64.0.9:2025"
     ];
-    extra-trusted-public-keys = [
-      "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
+    trusted-public-keys = [
+      "yoke-bin-cache:ddWddUNLU59tCn5o6xwweO88tXpcnJql6pqpF2aYkc4="
     ];
   };
   inputs = { nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable"; 

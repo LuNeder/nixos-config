f469097a24266f9275c4882a2d4b67241752ec2f
Luana <luana@luana.dev.br>
2026-02-01 15:03:21 -0300

    feat: NAS: yaiiu-immich-proxy

--- Yoke/services/immich.nix
+++ Yoke/services/immich.nix
@@ -259,6 +259,13 @@
     '';
   };
 
+  services.yaiiu-immich-proxy = {
+    enable = true;
+    port = 2282;
+    immichUrl = "http://localhost:${toString config.services.immich.port}";
+    openFirewall = true;
+  };
+
   users.users.immich.extraGroups = [ "personalfiles" "viddownload" ];
 
 

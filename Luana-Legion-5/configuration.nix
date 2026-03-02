0f58b5bacf49080c8318fa437b9e1a5066e79822
Luana <luana@luana.dev.br>
2026-03-02 14:59:26 -0300

    Dup

--- Luana-Legion-5/configuration.nix
+++ Luana-Legion-5/configuration.nix
@@ -430,20 +430,20 @@
     pipewire
     systemd
     vulkan-loader
-    xorg.libX11
-    xorg.libXScrnSaver
-    xorg.libXcomposite
-    xorg.libXcursor
-    xorg.libXdamage
-    xorg.libXext
-    xorg.libXfixes
-    xorg.libXi
-    xorg.libXrandr
-    xorg.libXrender
-    xorg.libXtst
-    xorg.libxcb
-    xorg.libxkbfile
-    xorg.libxshmfence
+    libX11
+    libXScrnSaver
+    libXcomposite
+    libXcursor
+    libXdamage
+    libXext
+    libXfixes
+    libXi
+    libXrandr
+    libXrender
+    libXtst
+    libxcb
+    libxkbfile
+    libxshmfence
     zlib
   ]);
 

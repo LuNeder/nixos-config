53e304d6e535cb36e571da6b542a9b884102961e
Luana <luananeder@gmail.com>
2026-06-14 12:09:04 -0300

    fix: Legion: remove broken package

--- Luana-Legion-5/configuration.nix
+++ Luana-Legion-5/configuration.nix
@@ -88,7 +88,7 @@
     pkgs.libreoffice-fresh
     # pkgs.labplot Broken
     pkgs.lenovo-legion
-    pkgs.veloren
+    # pkgs.veloren Broken
     pkgs.vlc
     pkgs.kdePackages.qtwebsockets # for KDE Home Assistant applet
     pkgs.kdePackages.kamera

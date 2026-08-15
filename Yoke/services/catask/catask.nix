92411e6934f58681d3e470a6d87bd8a962369188
Luana <luana@luana.dev.br>
2026-08-15 11:53:24 -0300

    fix(NAS): Boot fail

--- Yoke/services/catask/catask.nix
+++ Yoke/services/catask/catask.nix
@@ -1,6 +1,6 @@
 { config, pkgs, lib, inputs, ... }: {
   merpkgs.services.catask = {
-    #enable = true; # TODO: Fix python deps
+    enable = true; # TODO: Fix python deps
    # package = pkgs.merpkgs.catask;
     listenAddress = "[::]";
     port = 8220;

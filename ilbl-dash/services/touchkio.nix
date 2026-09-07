ace02093a2d113d712ce9a502a453e7522570d25
Luana <luana@luana.dev.br>
2026-09-07 17:50:16 -0300

    fix(ilbl-dash): fix build

--- ilbl-dash/services/touchkio.nix
+++ ilbl-dash/services/touchkio.nix
@@ -6,24 +6,42 @@ let
         version = "1.4.2";
 
         src = pkgs.fetchurl {
-          url = "https://github.com/leukipp/touchkio/releases/download/v${finalAttrs.version}/touchkio_${finalAttrs.version}_arm64.deb";
-          hash = "sha256-MryXbD/VX+wG/Q5JWV0a6bchbJkkvv2U2jv7wiZV8IY=";
+          url = "https://github.com/leukipp/touchkio/releases/download/v${finalAttrs.version}/touchkio-linux-arm64-${finalAttrs.version}.zip";
+          hash = "sha256-Jkdv/KcKxdc3uKa9K7yal3l5UGR55e1Pd6Ydrw7n/cU=";
         };
 
-        unpackCmd = "dpkg -x $curSrc source";
+        #unpackPhase = "dpkg -xR $src .";
 
         nativeBuildInputs = with pkgs; [
-          dpkg
+          #dpkg
+          unzip
           autoPatchelfHook
           qt5.wrapQtAppsHook
-          electron
+          #electron
+          nspr
+          nss
+          at-spi2-core
+          cups
+          cairo
+          gtk3
+          pango
+          libXdamage
+          libXrandr
+          mesa
+          alsa-lib
         ];
 
         installPhase = ''
           runHook preInstall
-
+          
+          chmod -R 755 ./*
           mkdir -p $out/bin
-          mv ./* $out/
+          mkdir -p $out/share/touchkio
+          mv ./* $out/share/touchkio/
+
+          makeWrapper "$out/share/touchkio/touchkio" "$out/bin/touchkio" \
+            --chdir "$out/share/touchkio" \
+            --prefix PATH : "${lib.makeBinPath finalAttrs.nativeBuildInputs}"
 
           runHook postInstall
         '';
@@ -49,4 +67,12 @@ in
     "network-online.target"
     "systemd-resolved.service"
   ];
+
+#  nixpkgs.overlays = [
+#    (final: prev: {
+#      libsecret = prev.libsecret.overrideAttrs {
+#        doCkeck = false;
+#      };
+#    })
+#  ];
 }
